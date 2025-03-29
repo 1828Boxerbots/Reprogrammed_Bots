17fea943b7896de33c8fad362564e61559f2d74c
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2025-03-29 08:52:41 -0700

    Skyler Sebold - Finished Drive
    Theoretically Finished Drive Subsystem with LAMBDAS and some reworking of methods, such as inverting right motors in constructor and centralizing all drive motors to stop if any motor controller temperature reaches an unstable degree.

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -3,6 +3,8 @@
 DriveSub::DriveSub()
 {
 //Acts as an Init
+m_driveMotorBackRight.SetInverted(ctre::phoenix::motorcontrol::InvertType::InvertMotorOutput);
+m_driveMotorFrontRight.SetInverted(ctre::phoenix::motorcontrol::InvertType::InvertMotorOutput);
 }
 
 DriveSub::~DriveSub()
@@ -23,34 +25,56 @@ void DriveSub::Periodic()
     frc::SmartDashboard::PutNumber("DriveFrontRight Motorcontroller Temperature", m_driveMotorFrontRight.GetTemperature());
 }
 
-void DriveSub::TankDrive(double Leftspeed, double Rightspeed)
+frc2::StartEndCommand DriveSub::TankDrive(double Leftspeed, double Rightspeed)
 {
-    if ((m_driveMotorBackLeft.GetTemperature() >= DriveConstants::kTempuratureLimit) || (m_driveMotorFrontLeft.GetTemperature() >= DriveConstants::kTempuratureLimit))
+    return frc2::StartEndCommand
+    (
+        //execute
+        [this, Leftspeed, Rightspeed]
+    {
+    if ((m_driveMotorBackLeft.GetTemperature() >= DriveConstants::kTempuratureLimit) || (m_driveMotorFrontLeft.GetTemperature() >= DriveConstants::kTempuratureLimit) || (m_driveMotorBackRight.GetTemperature() >= DriveConstants::kTempuratureLimit) || (m_driveMotorFrontRight.GetTemperature() >= DriveConstants::kTempuratureLimit))
     {
         m_driveMotorBackLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
         m_driveMotorFrontLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
-    }
-    else
-    {
-        m_driveMotorBackLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, Leftspeed);
-        m_driveMotorFrontLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, Leftspeed);
-    }
-    if ((m_driveMotorBackRight.GetTemperature() >= DriveConstants::kTempuratureLimit) || (m_driveMotorFrontRight.GetTemperature() >= DriveConstants::kTempuratureLimit))
-    {
         m_driveMotorBackRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
         m_driveMotorFrontRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
     }
     else
     {
-        m_driveMotorBackRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, -Rightspeed);
-        m_driveMotorFrontRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, -Rightspeed);
+        m_driveMotorBackLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, Leftspeed);
+        m_driveMotorFrontLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, Leftspeed);
+        m_driveMotorBackRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, Rightspeed);
+        m_driveMotorFrontRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, Rightspeed);
     }
+
+    },
+    //end
+    [this]
+    {
+        m_driveMotorBackLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
+        m_driveMotorFrontLeft.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
+        m_driveMotorBackRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
+        m_driveMotorFrontRight.Set(ctre::phoenix::motorcontrol::VictorSPXControlMode::PercentOutput, 0);
+    }
+    );
     
 }
 
-void DriveSub::RCDrive(double LeftY, double RightX)
+frc2::StartEndCommand DriveSub::RCDrive(double LeftY, double RightX)
 {
-    TankDrive(LeftY + RightX, LeftY - RightX);
+    return frc2::StartEndCommand 
+    (
+        //Execute
+        [this, LeftY, RightX]
+        {
+            TankDrive(LeftY + RightX, LeftY - RightX);
+        },
+        //End
+        [this]
+        {
+            TankDrive(0, 0);
+        }
+    );
 }
 
 
--- src/main/include/subsystems/DriveSubsystem.h
+++ src/main/include/subsystems/DriveSubsystem.h
@@ -3,6 +3,7 @@
 #include <frc2/command/SubsystemBase.h>
 #include <frc2/command/CommandPtr.h> 
 #include <frc2/command/RunCommand.h>
+#include <frc2/command/StartEndCommand.h>
 #include <ctre/phoenix/motorcontrol/can/VictorSPX.h>
 #include <frc/motorcontrol/VictorSP.h>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -17,8 +18,8 @@ DriveSub();
 
 void Init();
 void Periodic() override;
-void TankDrive(double Leftspeed, double Rightspeed);
-void RCDrive(double LeftY, double RightX);
+frc2::StartEndCommand TankDrive(double Leftspeed, double Rightspeed);
+frc2::StartEndCommand RCDrive(double LeftY, double RightX);
 
 
 //Unsure of motor number and configuration, double check the true orentation of motor number to position on robot
